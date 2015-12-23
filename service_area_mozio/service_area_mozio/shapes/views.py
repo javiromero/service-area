85cf58b5acb4a88380933d95c0d2a44863e4e889
Javi <javi.azuaga@gmail.com>
2015-12-23 23:27:01 +0100

    Improved docstrings. Fixed bad status code returned when error

--- service_area_mozio/service_area_mozio/shapes/views.py
+++ service_area_mozio/service_area_mozio/shapes/views.py
@@ -23,21 +23,30 @@ from service_area_mozio.shapes.mixins import JSONResponseMixin
 
 class PolygonListView(JSONResponseMixin, ListView):
 
-    """List all valid registered polygons"""
+    """List all valid registered polygons
+
+    Used a class based view here to show it's usage with models and mixins"""
     model = Polygon
     paginate_by = 5
 
     def render_to_response(self, context):
-        """Check format and reply accordingly"""
+        """Check format and reply accordingly in json or html"""
         if self.request.is_ajax():
-            return self.render_to_json_response(context)
+            json_response = self.render_to_json_response(
+                context['object_list'],
+                safe=False,
+            )
+            return json_response
         else:
             return super(PolygonListView, self).render_to_response(context)
 
 
 @csrf_exempt
 def create_polygon(request):
-    """Create a new Polygon"""
+    """Create a new Polygon
+
+    Used a function based view here to show it's usage with more complicated
+    requests"""
     if request.method == 'GET':
         allowed_methods = ['POST', ]
         return HttpResponseNotAllowed(allowed_methods)
@@ -50,17 +59,23 @@ def create_polygon(request):
             u' A valid service area should have 3 or more'
         )
         response = HttpResponse(msg)
-        response.status_code = 406
+        response.status_code = 400  # Bad request
     else:
+        # Get polygon or create a new one
         shape, created = Polygon.objects.get_or_create(name=name)
+        # Check if the vertices already belong to the shape or add them
+        # FIXME: check if vertices already belong to shape before creating
         for each in vertices:
             lat, lon = each.split(',')
             shape.vertices.create(lat=lat, lon=lon)
+
         if not created:
             msg = _(u'Service area updated')
+            status_code = 200  # Ok
         else:
             msg = _(u'Service area created')
+            status_code = 201  # Created
         response = HttpResponse(msg)
-        response.status_code = 201
+        response.status_code = status_code
 
     return response
